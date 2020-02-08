05c56cb26d44b7d37d831b7417efe0ce7316d022
lkbarreto <50026326+lkbarreto@users.noreply.github.com>
2020-02-08 07:38:56 -0500

    Add files via upload

---
+++ prueba/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    <v-footer fixed>
+      <v-spacer></v-spacer>
+      <div>Lourdes Barreto {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+
+  data() {
+    return {
+    };
+  }
+};
+</script>
---
+++ prueba/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <v-bottom-navigation v-model="bottomNav">
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header",
+
+  data() {
+    return {
+      bottomNav: "favorites"
+    };
+  }
+};
+</script>
